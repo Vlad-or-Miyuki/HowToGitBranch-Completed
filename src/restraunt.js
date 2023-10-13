0774daccd3684fb8169f15bfb185d5f7198c1a87
Miyuki <miyuki@Ubuntu.myguest.virtualbox.org>
2023-10-13 22:17:13 +0300

    Фикс 10 задания

--- src/restraunt.js
+++ src/restraunt.js
@@ -1,6 +1,6 @@
 const humans = 6;
 const bill = 3000;
-const endbill = 3500;
 const tea = 500;
-const divide = bill/humans
-console.log(`С каждого по 500 рублей, при общем счёте ${endbill} рублей и чаевыми ${tea} рублей`)
+const endbill = bill + tea
+const billpay = Math.floor(bill / humans)
+console.log(`С каждого по ${billpay} рублей,при общем счёте ${endbill} рублей и чаевыми ${tea} рублей`)
