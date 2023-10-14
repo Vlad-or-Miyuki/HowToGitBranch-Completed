9f215bd532e15aa975ffc8d55b2dd45b30e27769
Miyuki <miyuki@Ubuntu.myguest.virtualbox.org>
2023-10-14 15:07:13 +0300

    task 14 and file name fix

---
+++ src/task14.js
@@ -0,0 +1,7 @@
+const backslash = '//';
+const name = 'John';
+const firstname = 'Doe';
+const age = 25;
+
+const result = (`${backslash} Имя:      ${name} \n${backslash} Фамилия:  ${firstname}\n${backslash} Возраст:  ${age} Лет`)
+console.log(result)
